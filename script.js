53ca251da095845208d21a41f3fe525c4ce979d0
TylerBesecker <89785560+TylerBesecker@users.noreply.github.com>
2022-04-06 09:38:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -275,6 +275,12 @@ $('.keyboard-button').click(function(){
     
 });
 
+document.querySelectorAll("button").forEach( function(item) {
+    item.addEventListener('focus', function() {
+        this.blur();
+    })
+})
+
 const animateCSS = (element, animation, prefix = 'animate__') =>
     // We create a Promise and return it
     new Promise((resolve, reject) => {
